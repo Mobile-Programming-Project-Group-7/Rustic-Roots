892e37a3a8f79b778c3d2cf614d581353634b08c
Sujata Shrestha <shresthasujata054@gmail.com>
2023-04-01 19:18:33 +0300

    Splash Screen is created successfully

---
+++ app/src/main/java/com/example/rusticroots/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.rusticroots
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(3000){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+
+        }
+    }
+}
