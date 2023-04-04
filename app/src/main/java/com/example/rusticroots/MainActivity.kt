c9ed53c0ed6c85a95797539ac6b9262a32b11895
bebesf <bebes4sure@gmail.com>
2023-04-04 12:44:50 +0300

    backend

--- app/src/main/java/com/example/rusticroots/MainActivity.kt
+++ app/src/main/java/com/example/rusticroots/MainActivity.kt
@@ -1,13 +1,35 @@
     package com.example.rusticroots
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import com.example.rusticroots.Backend.BookingTable.BookingTable
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+
+    class MainActivity :ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+            val db = Firebase.firestore;
+            val user = hashMapOf(
+                "first" to "Adaddd",
+                "last" to "Lovelace",
+                "born" to 1815
+            )
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { d->
+                    Log.i( "DocumentSnapshot added with ID", "created successfully")
+                }
+                .addOnFailureListener { e ->
+                    Log.i("Error adding document", e.toString())
+                }
+        }
     }
-}
