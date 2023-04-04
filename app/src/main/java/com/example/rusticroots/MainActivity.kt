85fac05382923d4f1b05a4c717c3106102319268
Sujata Shrestha <shresthasujata054@gmail.com>
2023-04-04 17:40:12 +0300

    Splash Screen is created successfully

--- app/src/main/java/com/example/rusticroots/MainActivity.kt
+++ app/src/main/java/com/example/rusticroots/MainActivity.kt
@@ -10,19 +10,21 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.rusticroots.ui.theme.RusticRootsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            installSplashScreen()
             RusticRootsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Rustic Roots")
                 }
             }
         }
@@ -31,13 +33,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Welcome To $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RusticRootsTheme {
-        Greeting("Android")
+        Greeting("Rustic Roots")
     }
 }
