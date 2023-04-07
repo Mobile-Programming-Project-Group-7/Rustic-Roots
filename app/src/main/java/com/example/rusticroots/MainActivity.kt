e1d8c07c40c9ceb3dc67d2484b4d9c010bf17a29
ChrisVar95 <christina.vargka@gmail.com>
2023-04-07 11:06:20 +0300

    Fixed Merge issues

--- app/src/main/java/com/example/rusticroots/MainActivity.kt
+++ app/src/main/java/com/example/rusticroots/MainActivity.kt
@@ -1,35 +1,79 @@
-    package com.example.rusticroots
+package com.example.rusticroots
 
-import android.app.Application
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.Observer
+import com.example.rusticroots.ui.modules.GooglePayButton
+import com.example.rusticroots.ui.theme.RusticRootsTheme
+import com.example.rusticroots.viewmodel.PaymentGViewModel
 import com.example.rusticroots.Backend.BookingTable.BookingTable
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+        val paymentGVM: PaymentGViewModel by viewModels() // = PaymentGViewModel(this.application)
 
+        val db = Firebase.firestore;
+        val user = hashMapOf(
+            "first" to "Adaddd",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { d->
+                Log.i( "DocumentSnapshot added with ID", "created successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.i("Error adding document", e.toString())
+            }
 
-    class MainActivity :ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-
-
-            val db = Firebase.firestore;
-            val user = hashMapOf(
-                "first" to "Adaddd",
-                "last" to "Lovelace",
-                "born" to 1815
-            )
-            db.collection("users")
-                .add(user)
-                .addOnSuccessListener { d->
-                    Log.i( "DocumentSnapshot added with ID", "created successfully")
+        setContent {
+            RusticRootsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp(paymentGVM = paymentGVM)
                 }
-                .addOnFailureListener { e ->
-                    Log.i("Error adding document", e.toString())
-                }
+            }
+        }
+        // Check Google Pay availability
+        paymentGVM.canUseGooglePay.observe(this, Observer(::setGooglePayAvailable) )
+    }
+    /**
+     * If Google pay is not available, user gets notified
+     */
+    private fun setGooglePayAvailable(available: Boolean) {
+        if (available) {
+            Log.d("**********************","AVAILABLE")
+        } else {
+            Toast.makeText(
+                this,
+                getString(R.string.google_pay_NA),
+                Toast.LENGTH_LONG
+            ).show()
+            Log.e("**********************","NOT AVAILABLE")
         }
     }
+}
+
+@Composable
+fun MyApp(paymentGVM: PaymentGViewModel) {
+    Column() {
+        GooglePayButton(paymentGVM = paymentGVM)
+    }
+}
