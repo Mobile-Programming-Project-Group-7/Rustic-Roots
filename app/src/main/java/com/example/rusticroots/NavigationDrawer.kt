e18f8ef2513fc6cc3825aed8f5afcc959c5f8b72
Sujata Shrestha <shresthasujata054@gmail.com>
2023-04-09 20:47:32 +0300

    updated

--- app/src/main/java/com/example/rusticroots/NavigationDrawer.kt
+++ app/src/main/java/com/example/rusticroots/NavigationDrawer.kt
@@ -1,5 +1,4 @@
 package com.example.rusticroots
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -7,13 +6,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rusticroots.model.data.MenuItem
