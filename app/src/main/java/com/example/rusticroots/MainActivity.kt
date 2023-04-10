5147cded6081bf572d9c8d56649048cc35771359
Sujata Shrestha <shresthasujata054@gmail.com>
2023-04-10 19:29:39 +0300

    Commemting Payment Codes

--- app/src/main/java/com/example/rusticroots/MainActivity.kt
+++ app/src/main/java/com/example/rusticroots/MainActivity.kt
@@ -32,13 +32,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RusticRootsTheme {
-                val scaffoldState = rememberScaffoldState()
-                val scope = rememberCoroutineScope()
+                val scaffoldState=rememberScaffoldState()
+                val scope=rememberCoroutineScope()
                 Scaffold(
-                    scaffoldState= scaffoldState,
-                    topBar = {
+                    scaffoldState=scaffoldState,
+                    topBar={
                         AppBar(
-                            onNavigationIconClick = {
+                            onNavigationIconClick={
                                 scope.launch {
                                     scaffoldState.drawerState.open()
                                 }
@@ -46,43 +46,49 @@ class MainActivity : ComponentActivity() {
                             }
                         )
                     },
-                    drawerContent = {
+                    drawerContent={
                         DrawerHeader()
                         DrawerBody(
                             items=listOf(
-                                MenuItem("profile",
-                                    title = "Profile",
-                                    contentDescription = "Go to your profile page",
-                                    icon = Icons.Default.Person
+                                MenuItem(
+                                    "profile",
+                                    title="Profile",
+                                    contentDescription="Go to your profile page",
+                                    icon=Icons.Default.Person
                                 ),
-                                MenuItem("Home",
-                                    title = "Home",
-                                    contentDescription = "Go to home screen",
-                                    icon = Icons.Default.Home
+                                MenuItem(
+                                    "Home",
+                                    title="Home",
+                                    contentDescription="Go to home screen",
+                                    icon=Icons.Default.Home
                                 ),
-                                MenuItem("favourite",
-                                    title = "Orders",
-                                    contentDescription = "Your favourite orders",
-                                    icon = Icons.Default.Favorite
+                                MenuItem(
+                                    "favourite",
+                                    title="Orders",
+                                    contentDescription="Your favourite orders",
+                                    icon=Icons.Default.Favorite
                                 ),
-                                MenuItem("settings",
-                                    title = "Settings",
-                                    contentDescription = "Go to settings screen",
-                                    icon = Icons.Default.Settings
+                                MenuItem(
+                                    "settings",
+                                    title="Settings",
+                                    contentDescription="Go to settings screen",
+                                    icon=Icons.Default.Settings
                                 ),
-                                MenuItem("feedback",
-                                    title = "Feedback",
-                                    contentDescription = "Go to feedback screen",
-                                    icon = Icons.Default.Notifications
+                                MenuItem(
+                                    "feedback",
+                                    title="Feedback",
+                                    contentDescription="Go to feedback screen",
+                                    icon=Icons.Default.Notifications
                                 ),
 
-                                MenuItem("help",
-                                    title = "Help",
-                                    contentDescription = "Get help",
-                                    icon = Icons.Default.Info
+                                MenuItem(
+                                    "help",
+                                    title="Help",
+                                    contentDescription="Get help",
+                                    icon=Icons.Default.Info
                                 ),
                             ),
-                            onItemClick= {
+                            onItemClick={
                                 println("Clicked on ${it.title}")
                             }
                         )
@@ -91,31 +97,31 @@ class MainActivity : ComponentActivity() {
 
 
                 }
-              val paymentGVM: PaymentGViewModel by viewModels() // = PaymentGViewModel(this.application)
+                /*      val paymentGVM: PaymentGViewModel by viewModels() // = PaymentGViewModel(this.application)*/
 
 
-        val db = Firebase.firestore;
-        val user = hashMapOf(
-            "first" to "Adaddd",
-            "last" to "Lovelace",
-            "born" to 1815
-        )
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { d->
-                Log.i( "DocumentSnapshot added with ID", "created successfully")
-            }
-            .addOnFailureListener { e ->
-                Log.i("Error adding document", e.toString())
-            }
-
-
+                val db=Firebase.firestore;
+                val user=hashMapOf(
+                    "first" to "Adaddd",
+                    "last" to "Lovelace",
+                    "born" to 1815
+                )
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { d ->
+                        Log.i("DocumentSnapshot added with ID", "created successfully")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.i("Error adding document", e.toString())
+                    }
+// These four closing  bracket are kept because of commenting below payment codes.
+            }}}}
 
 
                 // A surface container using the 'background' color from the theme
 
 
-
+/*
                 Surface(
 
                     modifier = Modifier.fillMaxSize(),
@@ -153,3 +159,4 @@ fun MyApp(paymentGVM: PaymentGViewModel) {
         GooglePayButton(paymentGVM = paymentGVM)
     }
 }
+*/
